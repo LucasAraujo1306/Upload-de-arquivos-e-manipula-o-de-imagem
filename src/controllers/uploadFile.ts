a0e3fb31f9be629fb16c1b2b6016c237103da54c
LucasAraujo1306 <lucasisaac007@gmail.com>
2023-04-01 22:26:20 -0300

    types específicos para fields

--- src/controllers/uploadFile.ts
+++ src/controllers/uploadFile.ts
@@ -13,10 +13,28 @@ export const uploadsFile = async (req: Request, res: Response) => {
 }
 
 export const uploadMultiplo = async (req: Request, res: Response) => {
-    const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+    //const files = req.files as { [fieldname: string]: Express.Multer.File[] }; aceita tudo
+
+    /*type UploadTypes = {
+        [fieldname:string]:Express.Multer.File[]
+    }*/ //permiter aceita outros files.qualquer coisa 
+
+    /*type UploadTypes = {
+        avatar: Express.Multer.File[],
+        gallery: Express.Multer.File[]
+    }
+
+    const files = req.files as UploadTypes;*/ //pode passa direto o type
+
+    const files = req.files as {
+        avatar: Express.Multer.File[],
+        gallery: Express.Multer.File[]
+    }
+
     console.log('avatar:', files.avatar)
     console.log('gallery:', files.gallery)
 
     return res.json({})
 }
+
 
